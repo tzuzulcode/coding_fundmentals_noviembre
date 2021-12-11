9f18092b9b12263b7d232cb016eddfd26ebff46a
Tzuzul Code <mail@tzuzulcode.com>
2021-12-11 11:02:10 -0600

    Primer commit, fundamentos de programación y primeros pasos en POO

---
+++ js/listaimagenes.js
@@ -0,0 +1,12 @@
+var imagenes = [
+    "https://images.unsplash.com/photo-1637393934065-25cc36bca316?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+    "https://images.unsplash.com/photo-1633113088942-99089f4abffa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1637314697388-ffba61af4496?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1637355285235-34bbf19b22ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+]
+
+for (var i = 0; i < imagenes.length; i++) {
+    var imagen = document.createElement("img")
+    imagen.src = imagenes[i]
+    document.body.appendChild(imagen)
+}
